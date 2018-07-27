945f14a4b0261e02b8aba2c92910949a02a1bb73
abl-tree <ablamparas@gmail.com>
2018-07-27 23:33:25 +0800

    landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,21 +11,48 @@ License URL: http://creativecommons.org/licenses/by/3.0/
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-		<title>Tembang Bootstarp Website Template | Home :: w3layouts</title>
-		<link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" media="all" />
-		<link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" type="text/css" media="all">
+		<title>{{ config('app.name', 'Laravel') }} | Home</title>
+		@if (App::isLocal())
+			<link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" media="all" />
+			<link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" type="text/css" media="all">
+			<link href="{{ asset('css/popuo-box.css') }}" rel="stylesheet" type="text/css" media="all" />
+			<script src="{{ asset('js/jquery.min.js') }}"></script>
+			<!-- start-smoth-scrolling -->
+			<script type="text/javascript" src="{{ asset('js/move-top.js') }}"></script>
+			<script type="text/javascript" src="{{ asset('js/easing.js') }}"></script>
+			<!-- banner Slider starts Here -->
+			<script src="{{ asset('js/responsiveslides.min.js') }}"></script>
+			<!-- pop-up-box -->
+			<script type="text/javascript" src="{{ asset('js/modernizr.custom.min.js') }}"></script>    
+			<script type="text/javascript" src="{{ asset('js/jquery.magnific-popup.js') }}"></script>
+			<!--//pop-up-box-->
+		@elseif (!App::isLocal() && Request::server('HTTP_X_FORWARDED_PROTO') == 'http')
+			<link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" media="all" />
+			<link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" type="text/css" media="all">
+			<script src="{{ asset('js/jquery.min.js') }}" defer></script>
+			<!-- start-smoth-scrolling -->
+			<script type="text/javascript" src="{{ asset('js/move-top.js') }}" defer></script>
+			<script type="text/javascript" src="{{ asset('js/easing.js') }}" defer></script>
+			<!-- banner Slider starts Here -->
+			<script src="{{ asset('js/responsiveslides.min.js') }}" defer></script>
+		@else
+			<link href="{{ secure_asset('css/style.css') }}" rel="stylesheet" type="text/css" media="all" />
+			<link href="{{ secure_asset('css/bootstrap.css') }}" rel="stylesheet" type="text/css" media="all">
+			<script src="{{ secure_asset('js/jquery.min.js') }}" defer></script>
+			<!-- start-smoth-scrolling -->
+			<script type="text/javascript" src="{{ secure_asset('js/move-top.js') }}" defer></script>
+			<script type="text/javascript" src="{{ secure_asset('js/easing.js') }}" defer></script>
+			<!-- banner Slider starts Here -->
+			<script src="{{ asset('js/responsiveslides.min.js') }}" defer></script>
+		@endif
 		<!-- web-font -->
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,300,700,800,400,600' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Merriweather:400,700italic,400italic,300,900,700,900italic,300italic' rel='stylesheet' type='text/css'>
 		<!-- web-font -->
 		<!-- js -->
-		<script src="{{ asset('js/jquery.min.js') }}"></script>
 		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 		<!-- js -->
-		<!-- start-smoth-scrolling -->
-		<script type="text/javascript" src="{{ asset('js/move-top.js') }}"></script>
-		<script type="text/javascript" src="{{ asset('js/easing.js') }}"></script>
 		<script type="text/javascript">
 			jQuery(document).ready(function($) {
 				$(".scroll").click(function(event){		
@@ -42,14 +69,22 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			<!-- container -->
 			<div class="container">
 				<div class="header-logo">
-					<h1><a href="#">TEMBANG <span>Jawa</span></a></h1>
+					<h1><a href="#">{{ config('app.name', 'Laravel') }}</a></h1>
 				</div>
 				<div class="top-nav">
                 
                 @if (Route::has('login'))
                     <div class="top-right links">
                         @auth
-                            <a href="{{ url('/home') }}">Home</a>
+							<p>Go to <a class="Signup play-icon" href="{{ url('/home') }}">Admin Page</a> or<span> 
+							<a class="Signup play-icon" href="{{ route('logout') }}"
+								onclick="event.preventDefault();
+												document.getElementById('logout-form').submit();">
+								{{ __('Logout') }}</a></p>
+
+							<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+								@csrf
+							</form>
                         @else
                             <p>You are not Logged in, <a class="Signup play-icon popup-with-zoom-anim" href="#small-dialog3">Log in here </a>   or<span> <a class="Signup play-icon popup-with-zoom-anim" href="#small-dialog2">Register</a></P>
                         @endauth
@@ -58,9 +93,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					
 				</div>
 				<div class="clearfix"> </div>
-				<!-- banner Slider starts Here -->
-	  			<script src="{{ asset('js/responsiveslides.min.js') }}"></script>
-				 <script>
+				<script>
 					// You can also use "$(window).load(function() {"
 					$(function () {
 					  // Slideshow 4
@@ -79,7 +112,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					  });
 				
 					});
-				  </script>
+				</script>
 				<!--//End-slider-script -->
 				<div  id="top" class="callbacks_container">
 					<ul class="rslides" id="slider4">
@@ -142,17 +175,17 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			<div class="container">
 				<div class="top-header">
 					<div class="fixed-header">
-					<div class="sticky-logo">
-						<h1><a class="scroll" href="#home">TEMBANG <span>Jawa</a> </span></h1>
-					</div>	
-					<div class="sticky-nav">
-						<ul>
-							<li><a class="scroll active" href="#about">About</a></li>   
-							<li><a class="scroll" href="#event">Event</a></li>
-							<li><a class="scroll" href="#contact">Contact</a></li>
-						</ul>
-					</div>
-					<div class="clearfix"> </div>
+						<div class="sticky-logo">
+							<h1><a class="scroll" href="#home">{{ config('app.name', 'Laravel') }}</a></h1>
+						</div>	
+						<div class="sticky-nav">
+							<ul class="menu">
+								<li><a class="scroll about-nav" href="#about">About</a></li>   
+								<li><a class="scroll event-nav" href="#event">Event</a></li>
+								<li><a class="scroll contact-nav" href="#contact">Contact</a></li>
+							</ul>
+						</div>
+						<div class="clearfix"> </div>
 					</div>
 				</div>
 			</div>
@@ -171,18 +204,36 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					$(".top-header").removeClass("fixed");
 				}
 			 });
-			 
+
+			 $('.menu li a').click(function () {
+				//  alert('nice');
+				$('.menu li a').not(this).removeClass('active');
+				$(this).addClass('active');
+			});
+
+			$(document).scroll(function() {
+				// console.log('about', $(window).scrollTop() + $(window).height() > $('#about').position().top + $('.about-info').position().top && $(window).scrollTop() + $(window).height() < $('#about').position().top + $('#about').height());
+				// console.log('event', $(window).scrollTop() + $(window).height() > $('#event').position().top + $('.event-grids').position().top && $(window).scrollTop() + $(window).height() < $('#event').position().top + $('#event').height());
+
+				if ($(window).scrollTop() + $(window).height() > $('#about').position().top && $(window).scrollTop() + $(window).height() < $('#about').position().top + $('#about').height()) {
+					$('.menu li a').not('.menu li a.about-nav').removeClass('active');
+					$('.menu li a.about-nav').addClass('active');
+				} else if ($(window).scrollTop() + $(window).height() > $('#event').position().top && $(window).scrollTop() + $(window).height() < $('#event').position().top + $('#event').height()) {
+					$('.menu li a').not('.menu li a.event-nav').removeClass('active');
+					$('.menu li a.event-nav').addClass('active');
+				} else if ($(window).scrollTop() + $(window).height() > $('#contact').position().top) {
+					$('.menu li a').not('.menu li a.contact-nav').removeClass('active');
+					$('.menu li a.contact-nav').addClass('active');
+				} else {
+					$('.menu li a').removeClass('active');
+				}
+			});
 		});
 		</script>
 		<!-- /script-for sticky-nav -->
 		<!-- banner -->
 		<div class="banner">
 			<a class="play-icon popup-with-zoom-anim" href="#small-dialog"><span> </span></a>
-			<!-- pop-up-box -->
-					<script type="text/javascript" src="js/modernizr.custom.min.js"></script>    
-					<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
-					<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
-					<!--//pop-up-box-->
 				<div id="small-dialog" class="mfp-hide">
 					<iframe src="//player.vimeo.com/video/64701045" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 				</div>	
@@ -190,39 +241,41 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                     <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
                         @csrf
                         <div class="login">
-                            <h3>Login</h3>
+                            <h3>{{ __('Login') }}</h3>
                             <p>Enter your account details to login</p>
-                            <input type="email" placeholder="Email" value="{{ old('email') }}" required autofocus>			
+                            <input type="email" placeholder="Email" value="{{ old('email') }}" name="email" required autofocus>			
                             <input type="password" placeholder="Password" name="password" required>							
                             <input type="submit"  value="Submit"/>
                         </div>
                     </form>
 				</div>
 				<div id="small-dialog2" class="mfp-hide">
-					<div class="signup">
-						<h3>Register</h3>
-						<h4>Please Enter Your Details</h4>
-						<input type="text" value="First Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'First Name';}" />
-						<input type="text" value="Second Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Second Name';}" />
-						<input type="text" class="email" value="Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email';}"  />
-						<input type="text" class="password" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}"/>
-						<input type="text" class="email" value="Confirm password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Confirm password';}"  />
-						<input type="submit" value="Submit"/>
-					</div>
+                    <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
+                        @csrf
+						<div class="signup">
+							<h3>Register</h3>
+							<h4>Please Enter Your Details</h4>
+							<input type="text" class="email" placeholder="Name" name="name" required/>
+							<input type="text" class="email" placeholder="Email" name="email" required/>
+							<input type="password" class="password" placeholder="Password" name="password" required/>
+							<input type="password" class="password" placeholder="Confirm password" name="password_confirmation" required/>
+							<input type="submit" value="Submit"/>
+						</div>
+					</form>
 				</div>	
 				 <script>
 						$(document).ready(function() {
-						$('.popup-with-zoom-anim').magnificPopup({
-							type: 'inline',
-							fixedContentPos: false,
-							fixedBgPos: true,
-							overflowY: 'auto',
-							closeBtnInside: true,
-							preloader: false,
-							midClick: true,
-							removalDelay: 300,
-							mainClass: 'my-mfp-zoom-in'
-						});
+							$('.popup-with-zoom-anim').magnificPopup({
+								type: 'inline',
+								fixedContentPos: false,
+								fixedBgPos: true,
+								overflowY: 'auto',
+								closeBtnInside: true,
+								preloader: false,
+								midClick: true,
+								removalDelay: 300,
+								mainClass: 'my-mfp-zoom-in'
+							});
 																						
 						});
 				</script>	
@@ -315,159 +368,16 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					</div>
 					<div class="clearfix"> </div>
 				</div>
-				<div class="event-grids">
-					<div class="col-md-3 event-grid">
-						<div class="pic"> </div>
-						<ul>
-							<li class="hedding">Wembley Stadium</li>
-							<li class="date">2 December 2014</li>
-						</ul>
-						<div class="clearfix"> </div>
-					</div>
-					<div class="col-md-4 event-grid small-text">
-						<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.</p>
-					</div>
-					<div class="col-md-2 event-grid large-text">
-						<p class="text">$ 200 USD</p>
-					</div>
-					<div class="col-md-3 event-grid text-button">
-						<ul>
-							<li class="num">100 tickets Available</li>
-							<li class="button yellow"><a href="#">Buy Ticket</a></li>
-						</ul>
-					</div>
-					<div class="clearfix"> </div>
-				</div>
-				<div class="event-grids">
-					<div class="col-md-3 event-grid">
-						<div class="pic"> </div>
-						<ul>
-							<li class="hedding">Wembley Stadium</li>
-							<li class="date">2 December 2014</li>
-						</ul>
-						<div class="clearfix"> </div>
-					</div>
-					<div class="col-md-4 event-grid small-text">
-						<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.</p>
-					</div>
-					<div class="col-md-2 event-grid large-text">
-						<p class="text">$ 200 USD</p>
-					</div>
-					<div class="col-md-3 event-grid text-button">
-						<ul>
-							<li class="num">100 tickets Available</li>
-							<li class="button yellow"><a href="#">Buy Ticket</a></li>
-						</ul>
-					</div>
-					<div class="clearfix"> </div>
-				</div>
-				<div class="event-grids">
-					<div class="col-md-3 event-grid">
-						<div class="pic"> </div>
-						<ul>
-							<li class="hedding">Wembley Stadium</li>
-							<li class="date">2 December 2014</li>
-						</ul>
-						<div class="clearfix"> </div>
-					</div>
-					<div class="col-md-4 event-grid small-text">
-						<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.</p>
-					</div>
-					<div class="col-md-2 event-grid large-text">
-						<p class="text">$ 200 USD</p>
-					</div>
-					<div class="col-md-3 event-grid text-button">
-						<ul>
-							<li class="num">100 tickets Available</li>
-							<li class="button yellow"><a href="#">Buy Ticket</a></li>
-						</ul>
-					</div>
-					<div class="clearfix"> </div>
-				</div>
-				<div class="event-grids">
-					<div class="col-md-3 event-grid">
-						<div class="pic"> </div>
-						<ul>
-							<li class="hedding">Wembley Stadium</li>
-							<li class="date">2 December 2014</li>
-						</ul>
-						<div class="clearfix"> </div>
-					</div>
-					<div class="col-md-4 event-grid small-text">
-						<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.</p>
-					</div>
-					<div class="col-md-2 event-grid large-text">
-						<p class="text">$ 200 USD</p>
-					</div>
-					<div class="col-md-3 event-grid text-button">
-						<ul>
-							<li class="num">100 tickets Available</li>
-							<li class="button yellow"><a href="#">Buy Ticket</a></li>
-						</ul>
-					</div>
-					<div class="clearfix"> </div>
-				</div>
 			</div>
 			<!-- container -->
 		</div>
 		<!-- contact-events -->
-		<!-- portfolio -->
-		<div class="portfolio">
-			<!-- container -->
-			<div class="container">
-				<h3>Gallery</h3>
-				<div class="gallery-images">
-					<div class="gallery-image">
-						<img src="images/3.jpg" alt="" />
-						<div class="caption">
-							<span> </span>
-						</div>
-					</div>
-					<div class="gallery-image">
-						<img src="images/4.jpg" alt="" />
-						<div class="caption">
-							<span> </span>
-						</div>
-					</div>
-					<div class="gallery-image right">
-						<img src="images/5.jpg" alt="" />
-						<div class="caption">
-							<span> </span>
-						</div>
-					</div>
-					<div class="gallery-image">
-						<img src="images/7.jpg" alt="" />
-						<div class="caption">
-							<span> </span>
-						</div>
-					</div>
-					<div class="gallery-image">
-						<img src="images/6.jpg" alt="" />
-						<div class="caption">
-							<span> </span>
-						</div>
-					</div>
-					<div class="gallery-image right">
-						<img src="images/8.jpg" alt="" />
-						<div class="caption">
-							<span> </span>
-						</div>
-					</div>
-					<div class="clearfix"> </div>
-					<div class="gallery-text">
-					
-					</div>
-				</div>
-			</div>
-			<!-- container -->
-		</div>
-		<!-- portfolio -->
 		<!-- contact -->
 		<div id="contact" class="contact">
 			<!-- container -->
 			<div class="container">
 				<h3>Contact Us</h3>
-				<div class="col-md-7">
+				<div class="col-md-7 contact-info">
 					<div class="col-md-6">
 						<form>
 							<input type="text" class="text" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}">
@@ -509,21 +419,21 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		</div>
 		<!-- footer -->
 		<script type="text/javascript">
-									$(document).ready(function() {
-										/*
-										var defaults = {
-								  			containerID: 'toTop', // fading element id
-											containerHoverID: 'toTopHover', // fading element hover id
-											scrollSpeed: 1200,
-											easingType: 'linear' 
-								 		};
-										*/
-										
-										$().UItoTop({ easingType: 'easeOutQuart' });
-										
-									});
-								</script>
-									<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+			$(document).ready(function() {
+				/*
+				var defaults = {
+					containerID: 'toTop', // fading element id
+					containerHoverID: 'toTopHover', // fading element hover id
+					scrollSpeed: 1200,
+					easingType: 'linear' 
+				};
+				*/
+				
+				$().UItoTop({ easingType: 'easeOutQuart' });
+				
+			});
+		</script>
+		<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
 	<!-- content-Get-in-touch -->
 	</body>
 </html>
